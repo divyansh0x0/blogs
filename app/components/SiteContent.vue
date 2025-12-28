53844130f6a57e24531d8eda350b9216edc18762
divyansh0x0 <divyansh0x0@gmail.com>
2025-12-28 17:41:45 +0530

    Added sidebar containing site content

--- app/components/SiteContent.vue
+++ app/components/SiteContent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {buildFileTree} from "~/lib/buildtree";
 import TreeNode from "~/components/TreeNode.vue";
-
+import ArrowRight from '~icons/material-symbols/keyboard-arrow-right';
 const {data: queryResult} = await useAsyncData(
     'navigation',
     () => queryCollection("content").all()
@@ -17,16 +17,52 @@ const tree = computed(() => {
 
     return buildFileTree(paths);
 })
+const isCollapsed = ref(true);
 </script>
 <template>
-    <div>
+    <nav :class="{collapsed:isCollapsed}">
         <h2>Content</h2>
         <div v-if="tree">
             <TreeNode v-for="node in tree.children" :key="node.name" :node="node"/>
         </div>
-    </div>
+        <button class="edge-handle" @click="isCollapsed = !isCollapsed">
+            <ArrowRight class="icon"/>
+        </button>
+    </nav>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+nav{
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: var(--color-surface-container-highest);
+    height: 100vh;
+    width: fit-content;
+    max-width: 80vw;
+    transform: translateX(0);
+    transition: transform var(--transition-speed) ease;
 
+}
+.edge-handle{
+    position: absolute;
+    height: 100px;
+    width: fit-content;
+    background-color: var(--color-primary);
+    right: 0;
+    top: 50%;
+    transform: translate(100%,-50%);
+    .icon{
+        transform: rotate(180deg);
+        transition: transform var(--transition-speed) ease;
+    }
+}
+nav.collapsed{
+    transform: translateX(-100%);
+    .edge-handle{
+        .icon{
+            transform: rotate(0deg);
+        }
+    }
+}
 </style>
