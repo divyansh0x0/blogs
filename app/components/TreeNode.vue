f0c1703189bd41fbada22c92b5b99c5601de5244
divyansh0x0 <divyansh0x0@gmail.com>
2025-12-28 17:19:40 +0530

    Added collapsable site content

--- app/components/TreeNode.vue
+++ app/components/TreeNode.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import ArrowRight from '~icons/material-symbols/keyboard-arrow-right';
 import type {TreeNode} from "@/lib/buildtree";
+
+function folderClickHandler(e: MouseEvent) {
+    const html = (e.target as HTMLDivElement);
+    html.classList.toggle("collapsed");
+    console.log(html);
+    e.stopImmediatePropagation();
+}
 
 defineProps<{
     node: TreeNode
@@ -9,15 +17,15 @@ defineProps<{
 <template>
     <li>
         <NuxtLink class="file" v-if="node.type === 'file'" :to="node.path">
-            <h3>
+            <h4>
                 {{ node.name }}
-            </h3>
+            </h4>
             <p>
-                {{node.overview}}
+                {{ node.overview }}
             </p>
         </NuxtLink>
-        <div class="folder collapsed" @click = "(e)=>(e.target as HTMLDivElement).classList.toggle('collapsed')" v-else open>
-            <h3> {{ node.name }}</h3>
+        <div role="button" class="folder collapsed" @click="folderClickHandler" v-else>
+            <h3> <ArrowRight class="icon"/> {{ node.name }}</h3>
             <ul>
                 <TreeNode
                     v-for="child in Object.values(node.children)"
@@ -29,26 +37,68 @@ defineProps<{
     </li>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 ul {
     list-style: none;
     padding-left: var(--padding-lg);
-    border-left: 1px solid var(--color-tertiary);
 }
-li{
+
+li {
     width: 100%;
+    border-left: 1px solid var(--color-secondary);
 }
-.file{
+
+h3 {
+    text-transform:capitalize;
+    pointer-events: none;
+    align-content: center;
+
+}
+
+.file {
     display: block;
     padding: var(--padding-md);
-    transition: max-height var(--transition-speed) ease;
+    max-height: 100vh;
+    height: fit-content;
+    opacity: 1;
+    transition: max-height var(--transition-speed) ease,
 }
-.folder{
+
+.folder {
+    text-transform:capitalize;
     cursor: pointer;
+
+    max-height: 100vh;
+    height: fit-content;
+
+    opacity: 1;
     background-color: var(--color-overlay-container);
     overflow: hidden;
+
+    .icon{
+        transform: rotate(90deg);
+        transition: transform var(--transition-speed) ease;
+    }
+
 }
-p{
+.file,.folder{
+    transition: max-height var(--transition-speed) ease,
+    padding-top var(--transition-speed) ease,
+    padding-bottom var(--transition-speed) ease,
+    opacity var(--transition-speed) ease;
+}
+.folder.collapsed {
+    .icon{
+        transform: rotate(0deg);
+    }
+    .folder,.file {
+        max-height: 0;
+        padding-top:  0;
+        padding-bottom: 0;
+        opacity: 0;
+    }
+}
+p {
     color: var(--color-on-overlay);
 }
 </style>
