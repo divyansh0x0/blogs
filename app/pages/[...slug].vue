e561a87a9c885d46a0005a67807051990792b774
divyansh0x0 <divyansh0x0@gmail.com>
2026-01-02 02:29:12 +0530

    Fixed page alignment

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,29 +1,25 @@
 <script setup lang="ts">
 const route = useRoute()
 
-const {data: page} = await useAsyncData('page-' + route.path, () => {
+const { data: page } = await useAsyncData('page-' + route.path, () => {
     return queryCollection('content').path(route.path).first()
 })
 
 if (!page.value) {
-    throw createError({statusCode: 404, statusMessage: 'Page not found', fatal: true})
+    throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
 }
 </script>
 
 <template>
-    <main class="content-container" v-if="page">
-        <ContentRenderer class="content" :value="page.body"/>
-        <TableOfContent class="toc" v-if="page.body.toc && page.body.toc.links.length>0" :links="page.body.toc.links"/>
-    </main>
+    <div class="content-container" v-if="page">
+        <ContentRenderer class="content" :value="page.body" />
+        <TableOfContent class="toc" v-if="page.body.toc && page.body.toc.links.length > 0" :links="page.body.toc.links" />
+    </div>
 </template>
 <style scoped>
-.content-container{
+.content-container {
     position: relative;
     height: 100%;
     width: 100%;
-}
-
-.content{
-    margin-left: 2rem; /* hardcoded margin based on sidebar edge handle width*/
 }
 </style>
